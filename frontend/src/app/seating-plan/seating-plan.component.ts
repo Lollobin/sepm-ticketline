aff3e0ea8a5b53f7c98b6194ee11e9691a8cc33b
Christoph Baumann <cb@ipcb.at>
2022-04-25 17:57:05 +0200

    Added pixi.js library Added component for seating plan

---
+++ frontend/src/app/seating-plan/seating-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seating-plan',
+  templateUrl: './seating-plan.component.html',
+  styleUrls: ['./seating-plan.component.scss']
+})
+export class SeatingPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
