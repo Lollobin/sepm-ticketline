e816ec87eecae86289286894a076af22ca3c3f37
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-04-26 18:04:15 +0200

    Database set up

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Event.java
@@ -0,0 +1,97 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long eventId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private long duration;
+
+    private String category;
+
+    @Column(columnDefinition = "CLOB")
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Event event = (Event) o;
+        return eventId == event.eventId && duration == event.duration && Objects.equals(name,
+            event.name) && Objects.equals(category, event.category) && Objects.equals(
+            content, event.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventId, name, duration, category, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+            "eventId=" + eventId +
+            ", name='" + name + '\'' +
+            ", duration=" + duration +
+            ", category='" + category + '\'' +
+            ", content='" + content + '\'' +
+            '}';
+    }
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/SeatingPlanLayout.java
@@ -0,0 +1,61 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SeatingPlanLayout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long seatingPlanLayoutId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SeatingPlanLayout that = (SeatingPlanLayout) o;
+        return seatingPlanLayoutId == that.seatingPlanLayoutId && Objects.equals(
+            seatingLayoutPath, that.seatingLayoutPath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seatingPlanLayoutId, seatingLayoutPath);
+    }
+
+    @Override
+    public String toString() {
+        return "SeatingPlanLayout{" +
+            "seatingPlanLayoutId=" + seatingPlanLayoutId +
+            ", seatingLayoutPath='" + seatingLayoutPath + '\'' +
+            '}';
+    }
+
+    @Column(nullable = false)
+    private String seatingLayoutPath;
+
+    public long getSeatingPlanLayoutId() {
+        return seatingPlanLayoutId;
+    }
+
+    public void setSeatingPlanLayoutId(long seatingPlanLayoutId) {
+        this.seatingPlanLayoutId = seatingPlanLayoutId;
+    }
+
+    public String getSeatingLayoutPath() {
+        return seatingLayoutPath;
+    }
+
+    public void setSeatingLayoutPath(String seatingLayoutPath) {
+        this.seatingLayoutPath = seatingLayoutPath;
+    }
+}
