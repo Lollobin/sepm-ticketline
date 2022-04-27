cac6ef2f77f816e118932882d25010a3d4b85a38
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-04-27 18:22:07 +0200

    Made PIXI.js work

--- frontend/src/app/components/seating-plan/seating-plan.component.ts
+++ frontend/src/app/components/seating-plan/seating-plan.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Application, Graphics } from "pixi.js"
 @Component({
   selector: 'app-seating-plan',
   templateUrl: './seating-plan.component.html',
   styleUrls: ['./seating-plan.component.scss']
 })
 export class SeatingPlanComponent implements OnInit {
-
+  @ViewChild("pixiContainer") pixiContainer: ElementRef<HTMLDivElement>
   constructor() { }
-
+  ngAfterViewInit(){
+    console.log(this.pixiContainer.nativeElement)
+    
+    const app = new Application({resizeTo: this.pixiContainer.nativeElement})
+    this.pixiContainer.nativeElement.appendChild(app.view)
+    const testGraphics = new Graphics()
+    testGraphics.beginFill(0xAAAAAA)
+    testGraphics.drawCircle(100,100,100)
+    app.stage.addChild(testGraphics)
+  }
   ngOnInit(): void {
+
   }
 
 }
