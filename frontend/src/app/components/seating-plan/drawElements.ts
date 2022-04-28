d7efba4d76a128547523ec43b45d2dc983ccadd3
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-04-28 13:32:45 +0200

    Added basic drawing logic with testing sample

---
+++ frontend/src/app/components/seating-plan/drawElements.ts
@@ -0,0 +1,125 @@
+import { Graphics, Text } from "pixi.js";
+
+interface Location {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+interface Color {
+  baseColor: number;
+  strokeColor: number;
+}
+
+function drawPlus(color: Color) {
+  const w = 20;
+  const h = 20;
+  const scaleFactor = 0.2;
+  const plusContainer = drawArea({ x: 0, y: 0, w, h }, color, w);
+  const plusSign = new Graphics()
+    .lineStyle({ width: 3, color: color.strokeColor })
+    .moveTo(w / 2, h - h * scaleFactor)
+    .lineTo(w / 2, h * scaleFactor)
+    .moveTo(w - w * scaleFactor, h / 2)
+    .lineTo(w * scaleFactor, h / 2);
+  plusContainer.addChild(plusSign);
+  return plusContainer;
+}
+function drawMinus(color: Color) {
+    const w = 20;
+    const h = 20;
+    const scaleFactor = 0.2;
+    const plusContainer = drawArea({ x: 0, y: 0, w, h }, color, w);
+    const plusSign = new Graphics()
+      .lineStyle({ width: 3, color: color.strokeColor })
+      .moveTo(w - w * scaleFactor, h / 2)
+      .lineTo(w * scaleFactor, h / 2);
+    plusContainer.addChild(plusSign);
+    return plusContainer;
+}
+
+function calculateBoxCenterPoint(parentWidth: number, childWidth: number){
+    return (parentWidth - childWidth)/2
+}
+
+function centerText(text: Text, parentWidth: number){
+    text.anchor.set(0.5, 0)
+    text.setTransform(parentWidth/2)
+}
+
+function drawStandingArea(
+  location: Location,
+  color: Color,
+  numberOfSeatsAvailable: number,
+  numberOfSeatsUnavailable: number,
+  text?: string
+) {
+  const areaGraphics = drawArea(location, color, 0);
+  const seatAvailability = drawText(
+    `${numberOfSeatsUnavailable}/${numberOfSeatsAvailable}`,
+    15,
+    areaGraphics.width
+  );
+  seatAvailability.setTransform(calculateBoxCenterPoint(areaGraphics.width, seatAvailability.width), location.h*0.05);
+  areaGraphics.addChild(seatAvailability);
+  const plusMinusContainer = new Graphics()
+  const plus = drawPlus(color);
+  const minus = drawMinus(color);
+  const ticketCounter = drawText("0", 15, 100)
+  ticketCounter.anchor.set(0.5, 0)
+  plus.setTransform(areaGraphics.width/2 + areaGraphics.width/3 - plus.width)
+  minus.setTransform(areaGraphics.width/2 -areaGraphics.width/3 )
+  plusMinusContainer.addChild(plus);
+  plusMinusContainer.addChild(minus);
+  ticketCounter.setTransform(areaGraphics.width/2, 0)
+  plusMinusContainer.addChild(ticketCounter)
+  plusMinusContainer.setTransform(0, seatAvailability.position.y + seatAvailability.height + location.h * 0.05 )
+
+  areaGraphics.addChild(plusMinusContainer)
+  if (text) {
+    const additionalText = drawText(text, 15, areaGraphics.width) 
+    centerText(additionalText, areaGraphics.width)
+    additionalText.setTransform(additionalText.position.x, plusMinusContainer.position.y + plusMinusContainer.height + location.h * 0.05)
+    areaGraphics.addChild(additionalText);
+    
+}
+  return areaGraphics;
+}
+function drawNoSeatArea(location: Location, color: Color, text?: string) {
+  const areaGraphics = drawArea(location, color, 0);
+  if (text) {
+    const additionalText = drawText(text, 15, areaGraphics.width) 
+    centerText(additionalText, areaGraphics.width)
+    additionalText.setTransform(additionalText.position.x, location.h * 0.05)
+    areaGraphics.addChild(additionalText);
+  }
+  return areaGraphics;
+}
+function drawText(text: string, fontSize: number, maxWidth: number) {
+  const textGraphics = new Text(text, {
+    fontSize,
+    wordWrap: true,
+    wordWrapWidth: maxWidth,
+    breakWords: true,
+    align: "center",
+  });
+  return textGraphics;
+}
+function drawArea(location: Location, color: Color, radius: number) {
+  const areaGraphics = new Graphics();
+  areaGraphics
+    .beginFill(color.baseColor)
+    .lineStyle({ width: 3, color: color.strokeColor, alignment: 1 })
+    .setTransform(location.x, location.y)
+    .drawRoundedRect(0, 0, location.w, location.h, radius);
+  return areaGraphics;
+}
+
+export {
+  drawArea,
+  drawMinus,
+  drawNoSeatArea,
+  drawPlus,
+  drawStandingArea,
+  drawText,
+};
--- frontend/src/app/components/seating-plan/seating-plan.component.ts
+++ frontend/src/app/components/seating-plan/seating-plan.component.ts
@@ -1,25 +1,102 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Application, Graphics } from "pixi.js"
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Application, Graphics, Rectangle, Text, TextStyle } from "pixi.js";
+import { drawNoSeatArea, drawStandingArea, drawArea } from "./drawElements";
 @Component({
-  selector: 'app-seating-plan',
-  templateUrl: './seating-plan.component.html',
-  styleUrls: ['./seating-plan.component.scss']
+  selector: "app-seating-plan",
+  templateUrl: "./seating-plan.component.html",
+  styleUrls: ["./seating-plan.component.scss"],
 })
 export class SeatingPlanComponent implements OnInit {
-  @ViewChild("pixiContainer") pixiContainer: ElementRef<HTMLDivElement>
-  constructor() { }
-  ngAfterViewInit(){
-    console.log(this.pixiContainer.nativeElement)
-    
-    const app = new Application({resizeTo: this.pixiContainer.nativeElement})
-    this.pixiContainer.nativeElement.appendChild(app.view)
-    const testGraphics = new Graphics()
-    testGraphics.beginFill(0xAAAAAA)
-    testGraphics.drawCircle(100,100,100)
-    app.stage.addChild(testGraphics)
+  @ViewChild("pixiContainer") pixiContainer: ElementRef<HTMLDivElement>;
+  private seatHoverAlpha = 0.7;
+  constructor() {}
+  ngAfterViewInit() {
+    const app = new Application({
+      resizeTo: this.pixiContainer.nativeElement,
+      antialias: true,
+      backgroundAlpha: 0,
+    });
+    this.pixiContainer.nativeElement.appendChild(app.view);
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 30; y++) {
+        const seat = drawArea(
+          { x: x * 15+5, y: y * 15+5, w: 10, h: 10 },
+          { baseColor: 0xf0f0f0, strokeColor:  y>15? 0xaa0000: 0xaaaa00 },
+          4
+        );
+        this.addListeners(
+          seat,
+          () => {},
+          () => {},
+          () => {}
+        );
+        app.stage.addChild(seat);
+      }
+    }
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 30; y++) {
+        const seat = drawArea(
+          { x: x * 15+455, y: y * 15+5, w: 10, h: 10 },
+          { baseColor: 0xf0f0f0, strokeColor: y>15? 0xaa0000: 0xaaaa00 },
+          4
+        );
+        this.addListeners(
+          seat,
+          () => {},
+          () => {},
+          () => {}
+        );
+        app.stage.addChild(seat);
+      }
+    }
+    app.stage.addChild(
+      drawNoSeatArea(
+        { x: 200, y: 0, w: 200, h: 100 },
+        { baseColor: 0xf0f0f0, strokeColor: 0x000000 },
+        "STAGE, when there is more text there is more text and this is good"
+      )
+    );
+    app.stage.addChild(
+      drawStandingArea(
+        { x: 200, y: 240, w: 200, h: 200 },
+        { baseColor: 0xf0f0f0, strokeColor: 0x0000aa },
+        15,
+        10,
+        "STANDING SECTOR 1"
+      )
+    );
+    app.stage.addChild(
+      drawStandingArea(
+        { x: 200, y: 120, w: 200, h: 100 },
+        { baseColor: 0xf0f0f0, strokeColor: 0x00aaaa },
+        100,
+        15,
+        "STANDING SECTOR 2"
+      )
+    );
   }
-  ngOnInit(): void {
+  private updateStandingAreaSeats() {}
 
+  private addListeners(
+    graphics: Graphics,
+    mouseOverCallback: () => void,
+    mouseOutCallback: () => void,
+    clickCallback: () => void
+  ) {
+    graphics.interactive = true;
+    graphics.buttonMode = true;
+    graphics.on("mouseover", () => {
+      graphics.alpha = this.seatHoverAlpha;
+      mouseOverCallback();
+    });
+    graphics.on("mouseout", () => {
+      graphics.alpha = 1;
+      mouseOutCallback();
+    });
+    graphics.on("click", () => {
+      console.log("click");
+      clickCallback();
+    });
   }
-
+  ngOnInit(): void {}
 }
