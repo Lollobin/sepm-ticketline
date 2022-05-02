ac72523b31cb802e6be42fba1578ae21cad1505f
Lilla Almhofer <lilla.almhofer@tuwien.ac.at>
2022-05-02 22:26:57 +0200

    Frontendupdate after generation of new swaggerfile

--- frontend/src/app/generated-sources/openapi/model/user.ts
+++ frontend/src/app/generated-sources/openapi/model/user.ts
@@ -9,6 +9,7 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
+import { Address } from './address';
 import { Gender } from './gender';
 
 
@@ -18,10 +19,7 @@ export interface User {
     lastName: string;
     email: string;
     gender: Gender;
-    street: string;
-    zipCode: string;
-    city: string;
-    country: string;
+    address: Address;
     isLocked: boolean;
 }
 
