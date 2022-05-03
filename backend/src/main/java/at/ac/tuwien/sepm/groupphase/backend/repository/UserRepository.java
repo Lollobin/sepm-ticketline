4213752eb8a663e613d5417a2bc65df8be9edae4
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-05-03 22:57:03 +0200

    Changed update query for updating locked state of user.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
@@ -4,21 +4,26 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<ApplicationUser,Long> {
-   ApplicationUser findUserByEmail(String eMail);
-   ApplicationUser save(ApplicationUser user);
+public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findUserByEmail(String eMail);
+
+    ApplicationUser save(ApplicationUser user);
 
 
-   @Query("select a from ApplicationUser a where a.lockedAccount = true")
-   List<ApplicationUser> findByLockedState();
+    @Query("select a from ApplicationUser a where a.lockedAccount = true")
+    List<ApplicationUser> findByLockedState();
 
-   @Modifying(flushAutomatically = true)
-   @Query("update ApplicationUser  a set a.lockedAccount = :unlock where a.userId = :id")
-   int unlockApplicationUser(@Param("id") Long id, @Param("unlock") boolean unlock);
+    List<ApplicationUser> findByLockedAccountEquals(boolean lockedAccount);
+
+
+    @Transactional
+    @Modifying
+    @Query("update ApplicationUser a set a.lockedAccount = ?1 where a.userId = ?2")
+    int unlockApplicationUser(boolean lockedAccount, long userId);
 
 
 }
