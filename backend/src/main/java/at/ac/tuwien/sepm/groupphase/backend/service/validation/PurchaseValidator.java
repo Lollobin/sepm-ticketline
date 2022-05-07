4e36bb25166b8f71756d06edf5a54683552fd518
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-05-07 15:17:29 +0200

    Used correct error in PurcahseValidator

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/validation/PurchaseValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/validation/PurchaseValidator.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.service.validation;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.TicketStatusDto;
+import at.ac.tuwien.sepm.groupphase.backend.exception.ValidationException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,11 +10,11 @@ public class PurchaseValidator {
     public void validateTicketInformation(TicketStatusDto ticketStatusDto) {
         if (!ticketStatusDto.getPurchased().isEmpty()
             && !ticketStatusDto.getReserved().isEmpty()) {
-            throw new IllegalArgumentException(
+            throw new ValidationException(
                 "Currently only purchasing or reserving is allowed, not both");
         }
         if (ticketStatusDto.getPurchased().isEmpty() && ticketStatusDto.getReserved().isEmpty()) {
-            throw new IllegalArgumentException("At least one ticket has to be bought or reserved.");
+            throw new ValidationException("At least one ticket has to be bought or reserved.");
         }
     }
 }
