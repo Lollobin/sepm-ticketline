ca11691a104864a7638132408ebbbb64b6542939
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-05-09 14:23:26 +0200

    changed LocalDateTime to LocalDate in mapper and validator

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/helpmapper/DateMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/helpmapper/DateMapper.java
@@ -1,24 +1,26 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.helpmapper;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import org.mapstruct.Mapper;
 
 @Mapper
 public class DateMapper {
 
-    public OffsetDateTime asOffsetDateTime(LocalDateTime localDateTime) {
-        if (localDateTime != null) {
-            return localDateTime.atOffset(ZoneId.of("Europe/Vienna").getRules().getOffset(localDateTime));
+    public OffsetDateTime asOffsetDateTime(LocalDate localDate) {
+        if (localDate != null) {
+            return OffsetDateTime.of(localDate, LocalTime.MIDNIGHT, ZoneOffset.UTC);
         } else {
             return null;
         }
     }
 
-    public LocalDateTime asLocalDateTime(OffsetDateTime offsetDateTime) {
+    public LocalDate asLocalDateTime(OffsetDateTime offsetDateTime) {
         if (offsetDateTime != null) {
-            return offsetDateTime.toLocalDateTime();
+            return offsetDateTime.toLocalDate();
         } else {
             return null;
         }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/validator/ShowValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/validator/ShowValidator.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.validator;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Show;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ValidationException;
+import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 public class ShowValidator {
 
     public void checkIfShowCorrect(Show show) {
-        if (!show.getDate().isAfter(LocalDateTime.now())) {
+        if (!show.getDate().isAfter(LocalDate.now())) {
             throw new ValidationException("The show can not be in the past");
         }
     }
