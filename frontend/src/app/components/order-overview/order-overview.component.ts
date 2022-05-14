424da9daec78289688b106edc959d0d689b01248
11909390 <benno.kossatz@tuwien.ac.at>
2022-05-14 18:20:18 +0200

    implemented basic order overview frontend

--- frontend/src/app/components/order-overview/order-overview.component.ts
+++ frontend/src/app/components/order-overview/order-overview.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
-import {TicketsService, Transaction} from "../../generated-sources/openapi";
+import {Order, TicketsService} from "../../generated-sources/openapi";
+import sampleOrders from "./sampleOrders.json";
 
 @Component({
   selector: "app-order-overview",
@@ -7,12 +8,17 @@ import {TicketsService, Transaction} from "../../generated-sources/openapi";
   styleUrls: ["./order-overview.component.scss"]
 })
 export class OrderOverviewComponent implements OnInit {
-  transactions: Transaction[];
+  orders: Order[];
+  today = new Date();
 
   constructor(private ticketService: TicketsService) {
   }
 
   ngOnInit(): void {
+    // TODO: Add retreival of necessary data here (when backend is implemented)
+    // @ts-ignore
+    this.orders = sampleOrders;
+    /*
     this.ticketService.ordersGet().subscribe({
       next: data => {
         this.transactions = data;
@@ -24,5 +30,11 @@ export class OrderOverviewComponent implements OnInit {
         console.log("Received orders");
       }
     });
+     */
+  }
+
+  dateInFuture(date: string) {
+    const orderDate = new Date(date);
+    return orderDate > this.today;
   }
 }
