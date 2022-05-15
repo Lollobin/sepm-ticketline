7ac6fc554403d6e049e8ba96c1a0eb8d0bc92c88
11909390 <benno.kossatz@tuwien.ac.at>
2022-05-15 22:47:31 +0200

    added mapping function for order dto

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/TransactionMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/TransactionMapper.java
@@ -1,14 +1,16 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper;
 
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.BookingTypeDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.OrderDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.TransactionDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Transaction;
-import org.mapstruct.Mapper;
-
+import at.ac.tuwien.sepm.groupphase.backend.entity.enums.BookingType;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import org.mapstruct.Mapper;
 
 @Mapper
 public interface TransactionMapper {
@@ -21,6 +23,21 @@ public interface TransactionMapper {
     TransactionDto transActionToTransactionDto(Transaction transaction);
 
     List<TransactionDto> transActionToTransactionDto(List<Transaction> transaction);
+
+
+    BookingTypeDto bookingTypeToBookingTypeDto(BookingType bookingType);
+
+
+    default OrderDto transActionToOrderDto(Transaction transaction) {
+        OrderDto orderDto = new OrderDto();
+
+        orderDto.setType(bookingTypeToBookingTypeDto(
+            transaction.getBookedIns().iterator().next().getBookingType()));
+
+        return orderDto;
+    }
+
+    List<OrderDto> transActionToOrderDto(List<Transaction> transactions);
 
 
 }
