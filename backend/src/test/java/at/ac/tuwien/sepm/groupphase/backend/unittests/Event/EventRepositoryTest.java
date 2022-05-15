ccaf3bd0fa80f114ba57515b17b2bcbba7d8c552
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-05-15 20:44:38 +0200

    Added tests for events.

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/Event/EventRepositoryTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/Event/EventRepositoryTest.java
@@ -1,17 +1,27 @@
 package at.ac.tuwien.sepm.groupphase.backend.unittests.Event;
 
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT2_CATEGORY;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT2_CONTENT;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT2_DURATION;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT2_NAME;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT3_CATEGORY;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT3_CONTENT;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT3_DURATION;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT3_NAME;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_CATEGORY;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_CONTENT;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_DURATION;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepm.groupphase.backend.repository.EventRepository;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.List;
-
-import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -21,44 +31,44 @@ public class EventRepositoryTest {
     private EventRepository eventRepository;
 
     @Test
-    public void shouldSaveEventWithValidIdAndCorrectColumns(){
+    public void shouldSaveNewEvent() {
         Event testEvent = new Event();
         testEvent.setName(EVENT_NAME);
-        testEvent.setDuration(EVENT_DURATION.longValue());
+        testEvent.setDuration(EVENT_DURATION);
         testEvent.setContent(EVENT_CONTENT);
         testEvent.setCategory(EVENT_CATEGORY);
 
         Event event = eventRepository.save(testEvent);
 
         assertNotNull(testEvent.getEventId());
-        assertThat(event).hasFieldOrPropertyWithValue("duration", EVENT_DURATION.longValue());
+        assertThat(event).hasFieldOrPropertyWithValue("duration", EVENT_DURATION);
         assertThat(event).hasFieldOrPropertyWithValue("content", EVENT_CONTENT);
         assertThat(event).hasFieldOrPropertyWithValue("category", EVENT_CATEGORY);
         assertThat(event).hasFieldOrPropertyWithValue("name", EVENT_NAME);
     }
 
     @Test
-    public void shouldStoreAllEvents(){
+    public void shouldStoreAllEvents() {
         Event event1 = new Event();
         Event event2 = new Event();
         Event event3 = new Event();
 
         event1.setName(EVENT_NAME);
-        event1.setDuration(EVENT_DURATION.longValue());
+        event1.setDuration(EVENT_DURATION);
         event1.setContent(EVENT_CONTENT);
         event1.setCategory(EVENT_CATEGORY);
 
         eventRepository.save(event1);
 
         event2.setName(EVENT2_NAME);
-        event2.setDuration(EVENT2_DURATION.longValue());
+        event2.setDuration(EVENT2_DURATION);
         event2.setContent(EVENT2_CONTENT);
         event2.setCategory(EVENT2_CATEGORY);
 
         eventRepository.save(event2);
 
         event3.setName(EVENT3_NAME);
-        event3.setDuration(EVENT3_DURATION.longValue());
+        event3.setDuration(EVENT3_DURATION);
         event3.setContent(EVENT3_CONTENT);
         event3.setCategory(EVENT3_CATEGORY);
 
@@ -70,19 +80,19 @@ public class EventRepositoryTest {
     }
 
     @Test
-    public void shouldFindEventById(){
+    public void shouldFindEventById() {
         Event event2 = new Event();
         Event event3 = new Event();
 
         event2.setName(EVENT2_NAME);
-        event2.setDuration(EVENT2_DURATION.longValue());
+        event2.setDuration(EVENT2_DURATION);
         event2.setContent(EVENT2_CONTENT);
         event2.setCategory(EVENT2_CATEGORY);
 
         eventRepository.save(event2);
 
         event3.setName(EVENT3_NAME);
-        event3.setDuration(EVENT3_DURATION.longValue());
+        event3.setDuration(EVENT3_DURATION);
         event3.setContent(EVENT3_CONTENT);
         event3.setCategory(EVENT3_CATEGORY);
 
@@ -92,7 +102,7 @@ public class EventRepositoryTest {
 
         assertThat(foundEvent).isEqualTo(event2);
         assertThat(foundEvent.getName()).isEqualTo(EVENT2_NAME);
-        assertThat(foundEvent.getDuration()).isEqualTo(EVENT2_DURATION.longValue());
+        assertThat(foundEvent.getDuration()).isEqualTo(EVENT2_DURATION);
         assertThat(foundEvent.getCategory()).isEqualTo(EVENT2_CATEGORY);
         assertThat(foundEvent.getContent()).isEqualTo(EVENT2_CONTENT);
     }
--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/Event/EventServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/Event/EventServiceTest.java
@@ -1,5 +1,17 @@
 package at.ac.tuwien.sepm.groupphase.backend.unittests.Event;
 
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_CATEGORY;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_CONTENT;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_DURATION;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_INVALID_CATEGORY_LENGTH;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_INVALID_DURATION_LOWER;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_INVALID_DURATION_UPPER;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_INVALID_NAME;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_INVALID_NAME_LENGTH;
+import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.EVENT_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.EventWithoutIdDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.EventMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
@@ -7,7 +19,8 @@ import at.ac.tuwien.sepm.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepm.groupphase.backend.repository.EventRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.EventService;
 import at.ac.tuwien.sepm.groupphase.backend.service.impl.EventServiceImpl;
-import at.ac.tuwien.sepm.groupphase.backend.validator.EventValidator;
+import at.ac.tuwien.sepm.groupphase.backend.service.validation.EventValidator;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,15 +31,12 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-
 
 @ExtendWith(MockitoExtension.class)
 class EventServiceTest {
 
-    @Mock private EventRepository eventRepository;
+    @Mock
+    private EventRepository eventRepository;
     private final EventValidator eventValidator = new EventValidator();
     private EventService eventService;
     private final Event fakePersistedEvent = new Event();
@@ -37,18 +47,19 @@ class EventServiceTest {
 
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         eventService = new EventServiceImpl(eventRepository, eventValidator);
+        eventRepository.deleteAll();
     }
 
+
     @Test
-    void shouldSaveNewEvent(){
+    void shouldSaveNewEvent() {
 
         ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
 
-
         eventWithoutIdToSave.setName(EVENT_NAME);
-        eventWithoutIdToSave.setDuration(EVENT_DURATION);
+        eventWithoutIdToSave.setDuration(BigDecimal.valueOf(EVENT_DURATION));
         eventWithoutIdToSave.setCategory(EVENT_CATEGORY);
         eventWithoutIdToSave.setContent(EVENT_CONTENT);
 
@@ -59,62 +70,68 @@ class EventServiceTest {
         Event capturedEvent = eventArgumentCaptor.getValue();
 
         assertEquals(EVENT_NAME, capturedEvent.getName());
-        assertEquals(EVENT_DURATION.longValue(), capturedEvent.getDuration());
+        assertEquals(EVENT_DURATION, capturedEvent.getDuration());
         assertEquals(EVENT_CATEGORY, capturedEvent.getCategory());
         assertEquals(EVENT_CONTENT, capturedEvent.getContent());
 
     }
+
     @Test
-    void shouldNotSaveEventDueToName(){
+    void shouldNotSaveEventDueToName() {
 
         eventWithoutIdToSave.setName(EVENT_INVALID_NAME);
-        eventWithoutIdToSave.setDuration(EVENT_DURATION);
+        eventWithoutIdToSave.setDuration(BigDecimal.valueOf(EVENT_DURATION));
         eventWithoutIdToSave.setCategory(EVENT_CATEGORY);
         eventWithoutIdToSave.setContent(EVENT_CONTENT);
 
-
-        ValidationException exception = Assertions.assertThrows(ValidationException.class, () -> eventService.createEvent(eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+            () -> eventService.createEvent(
+                eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
         Assertions.assertEquals("Name of event can not be empty ", exception.getMessage());
 
     }
 
     @Test
-    void shouldNotSaveEventDueToDuration(){
+    void shouldNotSaveEventDueToDuration() {
 
         eventWithoutIdToSave.setName(EVENT_NAME);
-        eventWithoutIdToSave.setDuration(EVENT_INVALID_DURATION_LOWER);
+        eventWithoutIdToSave.setDuration(BigDecimal.valueOf(EVENT_INVALID_DURATION_LOWER));
         eventWithoutIdToSave.setCategory(EVENT_CATEGORY);
         eventWithoutIdToSave.setContent(EVENT_CONTENT);
 
         //testing if duration can be lower than 10
-        ValidationException exception = Assertions.assertThrows(ValidationException.class, () -> eventService.createEvent(eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
-        Assertions.assertEquals("Duration has to be at least 10 minutes and less than 6 hours (360 minutes)", exception.getMessage());
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+            () -> eventService.createEvent(
+                eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
+        Assertions.assertEquals(
+            "Duration has to be at least 10 minutes and less than 6 hours (360 minutes)",
+            exception.getMessage());
 
         //testing if duration can be higher than 360
 
-        eventWithoutIdToSave.setDuration(EVENT_INVALID_DURATION_UPPER);
-        exception = Assertions.assertThrows(ValidationException.class, () -> eventService.createEvent(eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
-        Assertions.assertEquals("Duration has to be at least 10 minutes and less than 6 hours (360 minutes)", exception.getMessage());
+        eventWithoutIdToSave.setDuration(BigDecimal.valueOf(EVENT_INVALID_DURATION_UPPER));
+        exception = Assertions.assertThrows(ValidationException.class,
+            () -> eventService.createEvent(
+                eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
+        Assertions.assertEquals(
+            "Duration has to be at least 10 minutes and less than 6 hours (360 minutes)",
+            exception.getMessage());
 
 
     }
 
     @Test
-    void shouldNotSaveEventDueToLength(){
+    void shouldNotSaveEventDueToLength() {
 
         eventWithoutIdToSave.setName(EVENT_INVALID_NAME_LENGTH);
-        eventWithoutIdToSave.setDuration(EVENT_DURATION);
+        eventWithoutIdToSave.setDuration(BigDecimal.valueOf(EVENT_DURATION));
         eventWithoutIdToSave.setCategory(EVENT_INVALID_CATEGORY_LENGTH);
         eventWithoutIdToSave.setContent(EVENT_CONTENT);
 
-
-        ValidationException exception = Assertions.assertThrows(ValidationException.class, () -> eventService.createEvent(eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
-        Assertions.assertEquals("Name of event is too long & Category contains too many characters", exception.getMessage());
+        ValidationException exception = Assertions.assertThrows(ValidationException.class,
+            () -> eventService.createEvent(
+                eventMapper.eventWithoutIdDtoToEvent(eventWithoutIdToSave)));
+        Assertions.assertEquals("Name of event is too long & Category contains too many characters",
+            exception.getMessage());
     }
-
-
-
-
-
-
 }
