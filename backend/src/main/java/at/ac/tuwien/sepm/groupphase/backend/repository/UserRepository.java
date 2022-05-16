4645cda37137ea470602938264c30fa1a657a113
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-05-16 14:21:55 +0200

    Added documentation for UserRepository.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
@@ -19,16 +19,29 @@ public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
     ApplicationUser findUserByEmail(String email);
 
     /**
-     * Saves a new User and the encapsuled Address entity to the database.
-     * All repository ooperations are cascaded to the Address entity.
+     * Saves a new User and the encapsuled Address entity to the database. All repository
+     * ooperations are cascaded to the Address entity.
      *
      * @param user Entity to be saved
      * @return Saved User with the generated ids for the User and the persisted Address Entity
      */
     ApplicationUser save(ApplicationUser user);
 
+    /**
+     * Gets all users where lockedAccount is equal to the parameter
+     *
+     * @param lockedAccount if true then only locked users will be returned and vice versa
+     * @return List of ApplicationsUsers based on lockedAccount
+     */
     List<ApplicationUser> findByLockedAccountEquals(boolean lockedAccount);
 
+    /**
+     * Unlocks a user
+     *
+     * @param lockedAccount if false user will be unlocked and loginTries will be reset to 0
+     *                      (locking not yet implemented)
+     * @param userId        specifies which user will be unlocked
+     */
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("update ApplicationUser a set a.lockedAccount = ?1 where a.userId = ?2")
