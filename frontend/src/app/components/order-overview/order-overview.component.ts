d1cdad59fa490bc20b1eed058e57c184115622b2
11909390 <benno.kossatz@tuwien.ac.at>
2022-05-17 15:35:22 +0200

    added display of tickets in order overview

--- frontend/src/app/components/order-overview/order-overview.component.ts
+++ frontend/src/app/components/order-overview/order-overview.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {Order, TicketsService} from "../../generated-sources/openapi";
+import {Order, TicketsService, TicketWithShowInfo} from "../../generated-sources/openapi";
 
 @Component({
   selector: "app-order-overview",
@@ -8,12 +8,25 @@ import {Order, TicketsService} from "../../generated-sources/openapi";
 })
 export class OrderOverviewComponent implements OnInit {
   orders: Order[];
+  tickets: TicketWithShowInfo[];
   today = new Date();
 
   constructor(private ticketService: TicketsService) {
   }
 
   ngOnInit(): void {
+    this.ticketService.ticketInfoGet().subscribe({
+      next: data => {
+        this.tickets = data;
+      },
+      error: error => {
+        console.error("Error getting tickets", error.message);
+      },
+      complete: () => {
+        console.log("Received tickets");
+      }
+    });
+
     this.ticketService.ordersGet().subscribe({
       next: data => {
         this.orders = data;
