2468dcc10c7a5c770dec33d270de3a0458a781c4
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-05-19 16:34:01 +0200

    Added: selection for seats and movement of all selected

--- frontend/src/app/components/seating-plan-editor/seating-plan-editor.component.ts
+++ frontend/src/app/components/seating-plan-editor/seating-plan-editor.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import { forEach } from "lodash";
 import { Application, Container, Graphics } from "pixi.js";
 import { Show, ShowInformation } from "src/app/generated-sources/openapi";
 import { addButtonListeners } from "src/app/shared_modules/seatingPlanEvents";
@@ -22,6 +23,7 @@ export class SeatingPlanEditorComponent implements AfterViewInit {
   @ViewChild("infoOverlay") infoOverlay: ElementRef<HTMLDivElement>;
   pixiApplication: Application;
   seatingPlan: SeatingPlan;
+  selectedElements: { [key: string]: { seatGraphics: Graphics; seatCover: Graphics } } = {};
   hoverInfo = undefined;
   @Input() set sectors(sectors: SectorBuilder[]) {
     this.seatingPlan = generateFromSectorBuilder(sectors);
@@ -46,8 +48,7 @@ export class SeatingPlanEditorComponent implements AfterViewInit {
     this.addDragAndDrop(this.pixiApplication.stage, this.seatingPlan);
   }
 
-  onDragStart(event, graphics: Graphics) {
-    this.dragStartEventData = event.data;
+  onDragStart(graphics: Graphics) {
     graphics.alpha = 0.5;
     this.dragging = graphics.name;
   }
@@ -55,20 +56,53 @@ export class SeatingPlanEditorComponent implements AfterViewInit {
   onDragEnd(graphics: Graphics) {
     graphics.alpha = 1;
     this.dragging = "";
-    this.dragStartEventData = null;
   }
 
-  onDragMove(graphics: Graphics) {
+  onDragMove(event, graphics: Graphics, cover?: Graphics) {
     if (this.dragging === graphics.name) {
-      const newPosition = this.dragStartEventData.getLocalPosition(graphics.parent);
+      const newPosition = event.data.getLocalPosition(graphics.parent);
+      forEach(this.selectedElements, ({ seatGraphics, seatCover }) => {
+        if (graphics.name === seatGraphics.name) return;
+        const xPositionDifference = seatGraphics.x - graphics.x;
+        const yPositionDifference = seatGraphics.y - graphics.y;
+
+        seatGraphics.x = newPosition.x + xPositionDifference;
+        seatGraphics.y = newPosition.y + yPositionDifference;
+        if (seatCover) {
+          seatCover.x = newPosition.x + xPositionDifference;
+          seatCover.y = newPosition.y + yPositionDifference;
+        }
+      });
       graphics.x = newPosition.x;
       graphics.y = newPosition.y;
+      if (cover) {
+        cover.x = newPosition.x;
+        cover.y = newPosition.y;
+      }
     }
   }
 
+  selectSeat(seatGraphics: Graphics, seatCover: Graphics) {
+    this.selectedElements[seatGraphics.name] = {
+      seatGraphics: seatGraphics as Graphics,
+      seatCover: seatCover as Graphics,
+    };
+    seatCover.visible = true;
+  }
+  deselectSeat(seatGraphics: Graphics, seatCover: Graphics) {
+    this.selectedElements[seatGraphics.name].seatCover.visible = false;
+    delete this.selectedElements[seatGraphics.name];
+  }
+  deselectAllSeats() {
+    forEach(this.selectedElements, (graphics) => {
+      graphics.seatCover.visible = false;
+    });
+    this.selectedElements = {};
+  }
   addDragAndDrop(stage: Container, seatingPlan: SeatingPlan) {
     seatingPlan.seats.forEach((seat) => {
       const seatGraphics = stage.getChildByName(generateSeatId(seat.id));
+      const seatCover = stage.getChildByName(`${generateSeatId(seat.id)}_cover`);
       if (!seatGraphics) {
         return;
       }
@@ -77,15 +111,24 @@ export class SeatingPlanEditorComponent implements AfterViewInit {
       addButtonListeners(seatGraphics as Graphics, {
         mouseover: () => {},
         mouseout: () => {},
-        click: () => {
-          console.log(seat.id);
-        },
+        click: () => {},
+      });
+      seatGraphics.on("click", (event) => {
+        if (event.data.originalEvent.ctrlKey) {
+          !this.selectedElements[seatGraphics.name]
+            ? this.selectSeat(seatGraphics as Graphics, seatCover as Graphics)
+            : this.deselectSeat(seatGraphics as Graphics, seatCover as Graphics);
+        } else if (this.selectedElements[seatGraphics.name] == undefined) {
+          this.deselectAllSeats();
+        }
       });
       seatGraphics
-        .on("pointerdown", (event) => this.onDragStart(event, seatGraphics as Graphics))
+        .on("pointerdown", () => this.onDragStart(seatGraphics as Graphics))
         .on("pointerup", () => this.onDragEnd(seatGraphics as Graphics))
         .on("pointerupoutside", () => this.onDragEnd(seatGraphics as Graphics))
-        .on("pointermove", () => this.onDragMove(seatGraphics as Graphics));
+        .on("pointermove", (event) =>
+          this.onDragMove(event, seatGraphics as Graphics, seatCover as Graphics)
+        );
     });
     seatingPlan.sectors.forEach((sector) => {
       const sectorGraphics = stage.getChildByName(generateStandingAreaId(sector.id));
@@ -102,10 +145,10 @@ export class SeatingPlanEditorComponent implements AfterViewInit {
         },
       });
       sectorGraphics
-      .on("pointerdown", (event) => this.onDragStart(event, sectorGraphics as Graphics))
-      .on("pointerup", () => this.onDragEnd(sectorGraphics as Graphics))
-      .on("pointerupoutside", () => this.onDragEnd(sectorGraphics as Graphics))
-      .on("pointermove", () => this.onDragMove(sectorGraphics as Graphics));
+        .on("pointerdown", () => this.onDragStart(sectorGraphics as Graphics))
+        .on("pointerup", () => this.onDragEnd(sectorGraphics as Graphics))
+        .on("pointerupoutside", () => this.onDragEnd(sectorGraphics as Graphics))
+        .on("pointermove", (event) => this.onDragMove(event, sectorGraphics as Graphics));
     });
   }
   ngAfterViewInit() {
