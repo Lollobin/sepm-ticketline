a168c7c1215a908c95485c5c4de2c434aab9e314
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-05-23 12:38:23 +0000

    Revert "implemented search query for events (working, still have to test event search..."
    This reverts commit 4eff4e809452524ff7ba95a81af2c75fc151ed90

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/EventServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/EventServiceImpl.java
@@ -48,8 +48,17 @@ public class EventServiceImpl implements EventService {
     public EventSearchResultDto search(EventSearchDto eventSearchDto, Pageable pageable) {
         LOGGER.debug("Searching for events with EventSearchDto: {}", eventSearchDto);
 
+        Long duration;
+        if (eventSearchDto.getDuration() == null) {
+            duration = null;
+        } else {
+            duration = Long.valueOf(eventSearchDto.getDuration());
+        }
+
+        LOGGER.debug(String.valueOf(eventSearchDto));
+
         Page<Event> eventPage = this.eventRepository.search(eventSearchDto.getName(), eventSearchDto.getContent(),
-            eventSearchDto.getDuration(), eventSearchDto.getCategory(), eventSearchDto.getLocation(),
+            duration, eventSearchDto.getCategory(), eventSearchDto.getLocation(),
             eventSearchDto.getArtist(), pageable);
 
         return setEventSearchResultDto(eventPage);
@@ -80,7 +89,7 @@ public class EventServiceImpl implements EventService {
         LOGGER.trace("Setting EventSearchResultDto values");
         EventSearchResultDto eventSearchResultDto = new EventSearchResultDto();
 
-        eventSearchResultDto.setEvents(eventPage.getContent().stream().map(eventMapper::eventToEventDto).toList());
+        eventSearchResultDto.setEvents(eventPage.get().map(eventMapper::eventToEventDto).toList());
         eventSearchResultDto.setNumberOfResults(eventPage.getNumberOfElements());
         eventSearchResultDto.setCurrentPage(eventPage.getNumber());
         eventSearchResultDto.setPagesTotal(eventPage.getTotalPages());
