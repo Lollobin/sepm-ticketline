b4cda4187a303a75bb3c8343d1353006fefeb1bc
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-05-23 14:43:50 +0200

    fixed checkstyle errors

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
@@ -29,34 +29,34 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     //        and (:location is null) or (location.locationId = :location)
     //        and (:artist is null) or (artist.artistId = :artist)
     //        """)
-//    @Query("""
-//        select distinct Event.eventId, Event.name, Event.category, Event.content, Event.duration from Event join Show on Event.eventId = Show.event.eventId
-//            join PlaysIn on Show.showId = PlaysIn.show.showId join Artist on PlaysIn.artist.artistId = Artist.artistId
-//            join SectorPrice on Show.showId = SectorPrice.show.showId join Sector sector on SectorPrice.sector.sectorId = sector.sectorId
-//            join SeatingPlan on sector.seatingPlan.seatingPlanId = SeatingPlan.seatingPlanId
-//            join Location on SeatingPlan.location.locationId = Location.locationId
-//        where ((:name is null) or (upper(Event.name) like upper(concat(:name, '%'))))
-//        and ((:duration is null) or (Event.duration between :duration - 15 and :duration + 15))
-//        and ((:content is null) or (upper(Event.content) like upper(concat('%', :content, '%'))))
-//        and ((:category is null) or (upper(Event.category) like upper(:category)))
-//        and ((:location is null) or (Location.locationId = :location))
-//        and ((:artist is null) or (Artist.artistId = :artist))
-//        """)
-//    @Query("""
-//            select distinct event1.eventId, event1.name, event1.category, event1.content, event1.duration from Event event1
-//                join Show show1 on event1.eventId = show1.event.eventId
-//                join PlaysIn playsIn1 on show1.showId = playsIn1.show.showId join Artist artist1 on playsIn1.artist.artistId = artist1.artistId
-//                join SectorPrice sectorPrice1 on show1.showId = sectorPrice1.show.showId join Sector sector1 on sectorPrice1.sector.sectorId = sector1.sectorId
-//                join SeatingPlan seatingPlan1 on sector1.seatingPlan.seatingPlanId = seatingPlan1.seatingPlanId
-//                join Location location1 on seatingPlan1.location.locationId = location1.locationId
-//            where (:name is null) or upper(event1.name) like concat(upper(:name), '%')
-//            and (:duration is null) or (event1.duration between :duration - 15 and :duration + 15)
-//            and (:content is null) or upper(event1.content) like upper(concat('%', :content, '%'))
-//            and (:category is null) or upper(event1.category) like upper(:category)
-//            and (:location is null) or (location1.locationId = :location)
-//            and (:artist is null) or (artist1.artistId = :artist)
-//            """)
-        @Query("""
+    //    @Query("""
+    //        select distinct Event.eventId, Event.name, Event.category, Event.content, Event.duration from Event join Show on Event.eventId = Show.event.eventId
+    //            join PlaysIn on Show.showId = PlaysIn.show.showId join Artist on PlaysIn.artist.artistId = Artist.artistId
+    //            join SectorPrice on Show.showId = SectorPrice.show.showId join Sector sector on SectorPrice.sector.sectorId = sector.sectorId
+    //            join SeatingPlan on sector.seatingPlan.seatingPlanId = SeatingPlan.seatingPlanId
+    //            join Location on SeatingPlan.location.locationId = Location.locationId
+    //        where ((:name is null) or (upper(Event.name) like upper(concat(:name, '%'))))
+    //        and ((:duration is null) or (Event.duration between :duration - 15 and :duration + 15))
+    //        and ((:content is null) or (upper(Event.content) like upper(concat('%', :content, '%'))))
+    //        and ((:category is null) or (upper(Event.category) like upper(:category)))
+    //        and ((:location is null) or (Location.locationId = :location))
+    //        and ((:artist is null) or (Artist.artistId = :artist))
+    //        """)
+    //    @Query("""
+    //            select distinct event1.eventId, event1.name, event1.category, event1.content, event1.duration from Event event1
+    //                join Show show1 on event1.eventId = show1.event.eventId
+    //                join PlaysIn playsIn1 on show1.showId = playsIn1.show.showId join Artist artist1 on playsIn1.artist.artistId = artist1.artistId
+    //                join SectorPrice sectorPrice1 on show1.showId = sectorPrice1.show.showId join Sector sector1 on sectorPrice1.sector.sectorId = sector1.sectorId
+    //                join SeatingPlan seatingPlan1 on sector1.seatingPlan.seatingPlanId = seatingPlan1.seatingPlanId
+    //                join Location location1 on seatingPlan1.location.locationId = location1.locationId
+    //            where (:name is null) or upper(event1.name) like concat(upper(:name), '%')
+    //            and (:duration is null) or (event1.duration between :duration - 15 and :duration + 15)
+    //            and (:content is null) or upper(event1.content) like upper(concat('%', :content, '%'))
+    //            and (:category is null) or upper(event1.category) like upper(:category)
+    //            and (:location is null) or (location1.locationId = :location)
+    //            and (:artist is null) or (artist1.artistId = :artist)
+    //            """)
+    @Query("""
             select distinct event1 from Event event1
             where ((:location is null) or event1.eventId in (
                 select event2.eventId from Event event2 join Show show1 on event2.eventId = show1.event.eventId
@@ -73,8 +73,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             and ((:content is null) or upper(event1.content) like upper(concat('%', :content, '%')))
             and ((:category is null) or upper(event1.category) like upper(:category))
             """)
-    Page<Event> search(
-        @Param(value = "name") String name, @Param(value = "content") String content, @Param(value = "duration") Integer duration,
+    Page<Event> search(@Param(value = "name") String name, @Param(value = "content") String content, @Param(value = "duration") Integer duration,
         @Param(value = "category") String category, @Param(value = "location") Integer location, @Param(value = "artist") Integer artist,
         Pageable pageable);
 
