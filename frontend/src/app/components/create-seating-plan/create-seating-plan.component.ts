8af56a1b9ea7634f6c174126123a114d07e33815
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-05-27 14:32:34 +0200

    Added: location information to seating plan overview

--- frontend/src/app/components/create-seating-plan/create-seating-plan.component.ts
+++ frontend/src/app/components/create-seating-plan/create-seating-plan.component.ts
@@ -10,7 +10,8 @@ import {
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { SeatingPlanEditorComponent } from "../seating-plan-editor/seating-plan-editor.component";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { LocationWithoutId, SeatingPlanWithoutId } from "src/app/generated-sources/openapi";
+import { LocationsService, LocationWithoutId, SeatingPlanWithoutId, Location } from "src/app/generated-sources/openapi";
+import { ActivatedRoute } from "@angular/router";
 
 type ClickElement =
   | {
@@ -21,24 +22,40 @@ type ClickElement =
   | { data: StaticElement; type: "StaticElement" };
 
 @Component({
-  selector: 'app-create-seating-plan',
-  templateUrl: './create-seating-plan.component.html',
-  styleUrls: ['./create-seating-plan.component.scss'],
+  selector: "app-create-seating-plan",
+  templateUrl: "./create-seating-plan.component.html",
+  styleUrls: ["./create-seating-plan.component.scss"],
 })
 export class CreateSeatingPlanComponent implements OnInit {
+  locationId: number;
   page: number;
   faXmark = faXmark;
   sectors: SectorBuilder[] = [];
   chosenElement: ClickElement;
   selectedSector: number;
   seatInformation: { [provisionalId: number]: { rowNumber: number; seatNumber: number } } = {};
+  location: Location = {
+    //TODO: DElete test data when location get interface works
+    locationId: 1,
+    name: "Capital Arena",
+    address: {
+      houseNumber: "12", street: "Imaginary street", city: "City Capital", country: "Imaginarium", zipCode: "1212"
+    } 
+
+  }
   @ViewChild(SeatingPlanEditorComponent) seatingPlanEditor: SeatingPlanEditorComponent;
 
-  constructor() {
-  }
+  constructor(private route: ActivatedRoute, private locationsService: LocationsService) {}
 
   ngOnInit(): void {
     this.page = 1;
+    this.route.params.subscribe((params) => {
+      this.locationId = params["id"];
+      this.locationsService.locationsIdGet(this.locationId).subscribe({next: (location)=>{
+        //TODO: Add location get interface
+        console.log(location)
+      }, error: ()=>{}})
+    });
   }
   removeSector(index: number) {
     this.sectors.splice(index, 1);
@@ -73,7 +90,7 @@ export class CreateSeatingPlanComponent implements OnInit {
       //TODO: Get locationId after saving location
       seatingPlanLayoutId: 0,
       sectors: [],
-      seats: []
+      seats: [],
     };
     //TODO: Send locationWithout ID
     //Get back ID of location
