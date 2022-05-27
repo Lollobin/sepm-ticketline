d6bbc58c786e344d213bc551bac55be69fba76ab
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-05-27 16:57:16 +0200

    ran npm run generate-rest in frontend

--- frontend/src/app/generated-sources/openapi/api/locations.service.ts
+++ frontend/src/app/generated-sources/openapi/api/locations.service.ts
@@ -238,6 +238,70 @@ export class LocationsService {
     }
 
     /**
+     * Gets the seating plans of a location
+     * @param id ID of the location that is retreived
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public locationsIdSeatingPlansGet(id: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<Array<SeatingPlan>>;
+    public locationsIdSeatingPlansGet(id: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<Array<SeatingPlan>>>;
+    public locationsIdSeatingPlansGet(id: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<Array<SeatingPlan>>>;
+    public locationsIdSeatingPlansGet(id: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
+        if (id === null || id === undefined) {
+            throw new Error('Required parameter id was null or undefined when calling locationsIdSeatingPlansGet.');
+        }
+
+        let localVarHeaders = this.defaultHeaders;
+
+        let localVarCredential: string | undefined;
+        // authentication (BearerAuth) required
+        localVarCredential = this.configuration.lookupCredential('BearerAuth');
+        if (localVarCredential) {
+            localVarHeaders = localVarHeaders.set('Authorization', 'Bearer ' + localVarCredential);
+        }
+
+        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (localVarHttpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'application/json'
+            ];
+            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        let localVarHttpContext: HttpContext | undefined = options && options.context;
+        if (localVarHttpContext === undefined) {
+            localVarHttpContext = new HttpContext();
+        }
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        return this.httpClient.get<Array<SeatingPlan>>(`${this.configuration.basePath}/locations/${encodeURIComponent(String(id))}/seatingPlans`,
+            {
+                context: localVarHttpContext,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * Creates a location.
      * @param locationWithoutId 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
--- frontend/src/app/generated-sources/openapi/model/eventSearch.ts
+++ frontend/src/app/generated-sources/openapi/model/eventSearch.ts
@@ -14,6 +14,7 @@ import { Category } from './category';
 
 export interface EventSearch { 
     name?: string;
+    content?: string;
     category?: Category;
     duration?: number;
     location?: number;
