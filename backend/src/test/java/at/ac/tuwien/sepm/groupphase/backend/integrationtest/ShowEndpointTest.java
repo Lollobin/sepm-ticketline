8bb0d7548fe4fbf0dbb56ecced4ff60bedb8c553
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-05-29 13:39:48 +0200

    changed timezone in insert_show.sql

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ShowEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ShowEndpointTest.java
@@ -255,7 +255,7 @@ class ShowEndpointTest {
         ZoneId zone = ZoneId.of("Europe/Berlin");
         ZoneOffset zoneOffSet = zone.getRules().getOffset(LocalDateTime.now());
 
-        OffsetDateTime date = OffsetDateTime.of(LocalDateTime.of(2022, 5, 25, 0, 0), zoneOffSet);
+        OffsetDateTime date = OffsetDateTime.of(LocalDateTime.of(2022, 5, 25, 0, 0), ZoneOffset.UTC);
 
         MvcResult mvcResult =
             this.mockMvc
