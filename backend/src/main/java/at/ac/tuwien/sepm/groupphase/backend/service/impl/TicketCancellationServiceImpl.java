14a90d9c9d550ff792e34749d16a6db4f02ae9cc
11909390 <benno.kossatz@tuwien.ac.at>
2022-06-01 11:56:39 +0200

    Implemented: backend for cancellation of purchased tickets

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketCancellationServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketCancellationServiceImpl.java
@@ -11,13 +11,12 @@ import at.ac.tuwien.sepm.groupphase.backend.security.AuthenticationUtil;
 import at.ac.tuwien.sepm.groupphase.backend.service.OrderService;
 import at.ac.tuwien.sepm.groupphase.backend.service.TicketCancellationService;
 import at.ac.tuwien.sepm.groupphase.backend.service.validation.CancellationValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service
 public class TicketCancellationServiceImpl implements TicketCancellationService {
@@ -31,8 +30,10 @@ public class TicketCancellationServiceImpl implements TicketCancellationService 
     private final AuthenticationUtil authenticationUtil;
     private final OrderService orderService;
 
-    public TicketCancellationServiceImpl(CancellationValidator cancellationValidator, TicketRepository ticketRepository, UserRepository userRepository, AuthenticationUtil authenticationUtil,
-                                         OrderService orderService) {
+    public TicketCancellationServiceImpl(CancellationValidator cancellationValidator,
+        TicketRepository ticketRepository, UserRepository userRepository,
+        AuthenticationUtil authenticationUtil,
+        OrderService orderService) {
         this.cancellationValidator = cancellationValidator;
         this.ticketRepository = ticketRepository;
         this.userRepository = userRepository;
@@ -44,43 +45,43 @@ public class TicketCancellationServiceImpl implements TicketCancellationService 
     public TicketStatusDto cancelTickets(TicketStatusDto ticketsToCancel) {
         LOGGER.debug("Started ticket cancellation with following tickets: {}", ticketsToCancel);
 
-        boolean purchaseMode = ticketsToCancel.getReserved().isEmpty();
         this.cancellationValidator.validateTicketInformation(ticketsToCancel);
 
+        BookingType bookingType = ticketsToCancel.getReserved().isEmpty() ? BookingType.CANCELLATION
+            : BookingType.DERESERVATION;
+
         List<Ticket> ticketList = ticketRepository.findAllById(
-            purchaseMode ? ticketsToCancel.getPurchased() : ticketsToCancel.getReserved());
+            bookingType == BookingType.CANCELLATION ? ticketsToCancel.getPurchased()
+                : ticketsToCancel.getReserved());
         ApplicationUser user = this.userRepository.findUserByEmail(
             authenticationUtil.getEmail());
-        List<Ticket> unavailableTickets = getUnavailableTickets(ticketList, user);
+        List<Ticket> unavailableTickets = getUnavailableTickets(ticketList, user, bookingType);
 
         if (!unavailableTickets.isEmpty()) {
             throw new ValidationException(unavailableTickets.size() + " ticket(s) not available");
         }
 
-        updateTicketStatus(purchaseMode, ticketList);
+        for (Ticket ticket : ticketList) {
+            ticket.setReservedBy(null);
+            ticket.setPurchasedBy(null);
+        }
+        ticketRepository.saveAllAndFlush(ticketList);
 
-        this.orderService.generateTransaction(ticketList, user,
-            purchaseMode ? BookingType.CANCELLATION : BookingType.DERESERVATION);
+        this.orderService.generateTransaction(ticketList, user, bookingType);
 
         return ticketsToCancel;
     }
 
-    private void updateTicketStatus(boolean purchaseMode, List<Ticket> ticketList) {
-        for (Ticket ticket : ticketList) {
-            ticket.setReservedBy(null);
-            if (purchaseMode) {
-                ticket.setPurchasedBy(null);
-            }
-        }
-        ticketRepository.saveAllAndFlush(ticketList);
-    }
-
-    private List<Ticket> getUnavailableTickets(List<Ticket> ticketList, ApplicationUser user) {
+    private List<Ticket> getUnavailableTickets(List<Ticket> ticketList, ApplicationUser user,
+        BookingType bookingType) {
         List<Ticket> unavailableTickets = new ArrayList<>();
         for (Ticket ticket : ticketList) {
-            if (ticket.getPurchasedBy() != null) {
-                unavailableTickets.add(ticket);
-            } else if (ticket.getReservedBy() != null) {
+            if (bookingType == BookingType.CANCELLATION) {
+                if (ticket.getPurchasedBy().getUserId() != user.getUserId()) {
+                    unavailableTickets.add(ticket);
+                }
+            }
+            if (bookingType == BookingType.DERESERVATION) {
                 if (ticket.getReservedBy().getUserId() != user.getUserId()) {
                     unavailableTickets.add(ticket);
                 }
@@ -88,5 +89,4 @@ public class TicketCancellationServiceImpl implements TicketCancellationService 
         }
         return unavailableTickets;
     }
-
 }
