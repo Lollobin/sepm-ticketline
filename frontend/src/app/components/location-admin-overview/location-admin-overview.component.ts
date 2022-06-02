c427007855055a24f1a140a2dc8767b8970f607b
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-06-02 10:09:55 +0200

    Added: location overview module and router

---
+++ frontend/src/app/components/location-admin-overview/location-admin-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-admin-overview',
+  templateUrl: './location-admin-overview.component.html',
+  styleUrls: ['./location-admin-overview.component.scss']
+})
+export class LocationAdminOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
