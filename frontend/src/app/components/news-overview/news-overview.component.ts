7cb6fa63ff148ef5961e9939ce17e8a5f7099868
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-09 21:29:09 +0200

    improved news overview

--- frontend/src/app/components/news-overview/news-overview.component.ts
+++ frontend/src/app/components/news-overview/news-overview.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Article, ArticlesService} from "../../generated-sources/openapi";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-news-overview',
@@ -10,21 +11,70 @@ export class NewsOverviewComponent implements OnInit {
 
   articles: Article[] = [];
   error: Error;
+  empty = false;
+  filterRead = null;
+  articleImages = {};
 
-  constructor(private articleService: ArticlesService) { }
 
+  constructor(public articleService: ArticlesService, private router: Router) {
+  }
 
 
   ngOnInit(): void {
 
-    this.articleService.articlesGet(false).subscribe({
-      next: response => {
+    this.filterRead = this.router.url.includes("read");
+
+    console.log(this.filterRead + " ist filterread");
+
+    this.articleService.articlesGet(this.filterRead).subscribe({
+      next: async response => {
         this.articles = response;
+        this.empty = response.length === 0;
+        for (const article of this.articles) {
+          try {
+            this.getImage(article.images[0], article.articleId);
+          } catch (error) {
+
+          }
+
+        }
+
+        console.log(response);
       },
       error: error => {
         this.error = error;
       }
     });
+  }
+
+  createImageFromBlob(image: Blob, id: number) {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.articleImages[id] = reader.result;
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+
+  getImage(id: number, articleId: number) {
+
+    if (!id) {
+      return;
+    }
+
+    this.articleService.imagesIdGet(id).subscribe({
+      next: image => {
+        this.createImageFromBlob(image, articleId);
+      },
+    });
+  }
+
+
+  public vanishEmpty(): void {
+    this.empty = null;
   }
 
 }
