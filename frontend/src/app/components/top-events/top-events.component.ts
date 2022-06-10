e41acb5a1ea735260e8d225f72c1d0bd6259a23c
11909390 <benno.kossatz@tuwien.ac.at>
2022-06-10 18:38:16 +0200

    added basic topEvents overview

--- frontend/src/app/components/top-events/top-events.component.ts
+++ frontend/src/app/components/top-events/top-events.component.ts
@@ -1,16 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import {EventsService} from "../../generated-sources/openapi";
+import {Component, OnInit} from "@angular/core";
+import {
+  Category,
+  EventsService,
+  EventWithTicketsSold,
+  TopEventSearch
+} from "../../generated-sources/openapi";
 
 @Component({
-  selector: 'app-top-events',
-  templateUrl: './top-events.component.html',
-  styleUrls: ['./top-events.component.scss']
+  selector: "app-top-events",
+  templateUrl: "./top-events.component.html",
+  styleUrls: ["./top-events.component.scss"]
 })
 export class TopEventsComponent implements OnInit {
 
-  constructor(private eventsService: EventsService) { }
+  error = undefined;
+  topEvents: EventWithTicketsSold[];
+  month: Date = null;
+  category: Category = null;
+
+  constructor(private eventsService: EventsService) {
+  }
 
   ngOnInit(): void {
+    this.month = new Date(Date.now());
+    this.reloadEvents();
+  }
+
+  reloadEvents() {
+    const search: TopEventSearch = {
+      month: this.month.toISOString().split("T")[0],
+      category: this.category
+    };
+
+    this.eventsService.topEventsGet(search).subscribe({
+      next: data => {
+        this.topEvents = data;
+      },
+      error: error => {
+        console.error("Error getting top events", error.message);
+        this.setError(error);
+      },
+      complete: () => {
+        console.log("Received events");
+      }
+    });
+  }
+
+  setError(error: any) {
+    this.error = error;
   }
 
 }
