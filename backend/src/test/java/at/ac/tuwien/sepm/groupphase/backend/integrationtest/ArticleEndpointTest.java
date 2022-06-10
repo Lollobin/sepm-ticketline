ce8454e619bfa0ef81a2125d7dae345d41effcf6
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-11 01:54:38 +0200

    Added and fixed tests for creation of images and articles

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ArticleEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ArticleEndpointTest.java
@@ -11,14 +11,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import at.ac.tuwien.sepm.groupphase.backend.config.properties.SecurityProperties;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ArticleWithoutIdDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Article;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Image;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ArticleRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.FileSystemRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.ImageRepository;
 import at.ac.tuwien.sepm.groupphase.backend.security.JwtTokenizer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -56,9 +60,17 @@ class ArticleEndpointTest {
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private FileSystemRepository fileSystemRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+
     @BeforeEach
     void setUp() {
         articleRepository.deleteAll();
+        imageRepository.deleteAll();
     }
 
     @Test
@@ -80,7 +92,7 @@ class ArticleEndpointTest {
                     .header(
                         securityProperties.getAuthHeader(),
                         jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
-            .andDo(print())
+
             .andReturn();
         MockHttpServletResponse response = mvcResult.getResponse();
 
@@ -117,11 +129,146 @@ class ArticleEndpointTest {
                     .header(
                         securityProperties.getAuthHeader(),
                         jwtTokenizer.getAuthToken(ADMIN_USER, USER_ROLES)))
-            .andDo(print())
+
             .andReturn();
         MockHttpServletResponse response = mvcResult.getResponse();
 
         assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
 
+    }
+
+    @Test
+    void postWithOneImage_shouldReturn201() throws Exception {
+        String fakeImage = "das ist ein Teststring";
+        String filePath = fileSystemRepository.save(fakeImage.getBytes(StandardCharsets.UTF_8),
+            fakeImage);
+
+        Image image = new Image();
+        image.setFilePath(filePath);
+
+        imageRepository.save(image);
+
+        Long id = imageRepository.findAll().get(0).getImageId();
+
+        ArticleWithoutIdDto articleWithoutIdDto = new ArticleWithoutIdDto();
+
+        articleWithoutIdDto.setTitle(ARTICLE_TITLE);
+        articleWithoutIdDto.setSummary(ARTICLE_SUMMARY);
+        articleWithoutIdDto.setText(ARTICLE_TEXT);
+        List<Long> imageIds = new ArrayList<>();
+        imageIds.add(id);
+        articleWithoutIdDto.setImages(imageIds);
+
+        String json = objectMapper.writeValueAsString(articleWithoutIdDto);
+
+        MvcResult mvcResult = this.mockMvc
+            .perform(
+                post(ARTICLES_BASE_URI)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+                    .header(
+                        securityProperties.getAuthHeader(),
+                        jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
+            .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+
+        List<Article> persistedArticles = articleRepository.findAll();
+        List<Image> persistedImages = imageRepository.findAll();
+
+        assertThat(persistedArticles).hasSize(1);
+        assertThat(persistedImages).hasSize(1);
+
+        Article article = persistedArticles.get(0);
+
+        assertAll(
+            () -> assertEquals(ARTICLE_SUMMARY, article.getSummary()),
+            () -> assertEquals(ARTICLE_TITLE, article.getTitle()),
+            () -> assertEquals(ARTICLE_TEXT, article.getText())
+        );
+
+        assertThat(persistedImages.get(0).getArticle().getArticleId()).isEqualTo(
+            article.getArticleId());
+    }
+
+    @Test
+    void postWithMultipleImages_shouldReturn201() throws Exception {
+        String fakeImage = "image1";
+        String filePath = fileSystemRepository.save(fakeImage.getBytes(StandardCharsets.UTF_8),
+            fakeImage);
+
+        String fakeImage2 = "image2";
+        String filePath2 = fileSystemRepository.save(fakeImage2.getBytes(StandardCharsets.UTF_8),
+            fakeImage2);
+
+        String fakeImage3 = "image3";
+        String filePath3 = fileSystemRepository.save(fakeImage3.getBytes(StandardCharsets.UTF_8),
+            fakeImage3);
+
+        Image image = new Image();
+        image.setFilePath(filePath);
+
+        Image image2 = new Image();
+        image2.setFilePath(filePath2);
+
+        Image image3 = new Image();
+        image3.setFilePath(filePath3);
+
+        imageRepository.save(image);
+        imageRepository.save(image2);
+        imageRepository.save(image3);
+
+        Long id = imageRepository.findAll().get(0).getImageId();
+        Long id2 = imageRepository.findAll().get(1).getImageId();
+        Long id3 = imageRepository.findAll().get(2).getImageId();
+
+        ArticleWithoutIdDto articleWithoutIdDto = new ArticleWithoutIdDto();
+
+        articleWithoutIdDto.setTitle(ARTICLE_TITLE);
+        articleWithoutIdDto.setSummary(ARTICLE_SUMMARY);
+        articleWithoutIdDto.setText(ARTICLE_TEXT);
+        List<Long> imageIds = new ArrayList<>();
+        imageIds.add(id);
+        imageIds.add(id2);
+        imageIds.add(id3);
+        articleWithoutIdDto.setImages(imageIds);
+
+        String json = objectMapper.writeValueAsString(articleWithoutIdDto);
+
+        MvcResult mvcResult = this.mockMvc
+            .perform(
+                post(ARTICLES_BASE_URI)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+                    .header(
+                        securityProperties.getAuthHeader(),
+                        jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
+
+            .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+
+        List<Article> persistedArticles = articleRepository.findAll();
+        List<Image> persistedImages = imageRepository.findAll();
+
+        assertThat(persistedArticles).hasSize(1);
+        assertThat(persistedImages).hasSize(3);
+
+        Article article = persistedArticles.get(0);
+
+        assertAll(
+            () -> assertEquals(ARTICLE_SUMMARY, article.getSummary()),
+            () -> assertEquals(ARTICLE_TITLE, article.getTitle()),
+            () -> assertEquals(ARTICLE_TEXT, article.getText())
+        );
+
+        assertThat(persistedImages.get(0).getArticle().getArticleId()).isEqualTo(
+            article.getArticleId());
+        assertThat(persistedImages.get(1).getArticle().getArticleId()).isEqualTo(
+            article.getArticleId());
+        assertThat(persistedImages.get(2).getArticle().getArticleId()).isEqualTo(
+            article.getArticleId());
     }
 }
--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ImagesEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/ImagesEndpointTest.java
@@ -5,72 +5,79 @@ import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.ADMIN_USER;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import at.ac.tuwien.sepm.groupphase.backend.config.properties.SecurityProperties;
-import at.ac.tuwien.sepm.groupphase.backend.repository.ArticleRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.ImageRepository;
 import at.ac.tuwien.sepm.groupphase.backend.security.JwtTokenizer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-public class ImagesEndpointTest {
+class ImagesEndpointTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
 
-    @Autowired
-    private ArticleRepository articleRepository;
 
     @Autowired
     private JwtTokenizer jwtTokenizer;
 
+
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private ImageRepository imageRepository;
 
     @Autowired
     private SecurityProperties securityProperties;
 
     @BeforeEach
     void setUp() {
-        articleRepository.deleteAll();
+        imageRepository.deleteAll();
     }
 
     @Test
     void postWithCorrectImage_shouldReturn201() throws Exception {
-//        MockMultipartFile file = new MockMultipartFile("image", "", "application/json", "test/image.data".getBytes());
-//
-//        MvcResult mvcResult = this.mockMvc
-//            .perform(MockMvcRequestBuilders.multipart("/images").file(file).header(
-//                    securityProperties.getAuthHeader(),
-//                    jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES))
-//                ).andReturn();
-//        MockHttpServletResponse response = mvcResult.getResponse();
-//
-//        assertEquals(response.getStatus(), HttpStatus.CREATED.value());
-
-        MockMultipartFile employeeJson = new MockMultipartFile("employee", null,
-            "image/png", "{\"name\": \"Emp Name\"}".getBytes());
 
         mockMvc.perform(MockMvcRequestBuilders.multipart("/images")
-                .file(employeeJson)
+                .file(new MockMultipartFile("fileName", "testfile.jpg", MediaType.IMAGE_JPEG_VALUE,
+                    "test/image.data".getBytes()))
                 .header(
                     securityProperties.getAuthHeader(),
                     jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
-            .andExpect(status().isOk());
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void postWithWrongRole_shouldReturn403() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/images")
+                .file(new MockMultipartFile("fileName", "testfile.jpg", MediaType.IMAGE_JPEG_VALUE,
+                    "test/image.data".getBytes()))
+            )
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void postWithWrongRoleMimeType_shouldReturn422() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/images")
+                .file(
+                    new MockMultipartFile("fileName", "testfile.jpg", MediaType.APPLICATION_JSON_VALUE,
+                        "test/image.data".getBytes())).header(
+                    securityProperties.getAuthHeader(),
+                    jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES))
+            )
+            .andExpect(status().isUnprocessableEntity());
     }
 
 }
