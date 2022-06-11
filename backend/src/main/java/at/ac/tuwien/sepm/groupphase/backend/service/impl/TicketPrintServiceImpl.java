5673941a5be46f82cc31c05865e7db7d42f9f6e3
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-06-11 19:16:53 +0200

    Added: Basic PDF creation for tickets

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketPrintServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketPrintServiceImpl.java
@@ -7,6 +7,14 @@ import static at.ac.tuwien.sepm.groupphase.backend.config.Constants.COMPANY_NAME
 import static at.ac.tuwien.sepm.groupphase.backend.config.Constants.COMPANY_PHONE;
 import static at.ac.tuwien.sepm.groupphase.backend.config.Constants.COMPANY_WEB;
 
+import at.ac.tuwien.sepm.groupphase.backend.entity.Address;
+import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Artist;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Location;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Ticket;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepm.groupphase.backend.repository.TicketRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
 import at.ac.tuwien.sepm.groupphase.backend.security.AuthenticationUtil;
@@ -15,7 +23,10 @@ import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
 import java.util.Date;
+import java.util.Optional;
+import java.util.Set;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -39,8 +50,7 @@ public class TicketPrintServiceImpl implements TicketPrintService {
     private final UserRepository userRepository;
 
     public TicketPrintServiceImpl(TicketRepository ticketRepository,
-        AuthenticationUtil authenticationFacade,
-        UserRepository userRepository) {
+        AuthenticationUtil authenticationFacade, UserRepository userRepository) {
         this.ticketRepository = ticketRepository;
         this.authenticationFacade = authenticationFacade;
         this.userRepository = userRepository;
@@ -48,15 +58,27 @@ public class TicketPrintServiceImpl implements TicketPrintService {
 
     @Override
     public Resource getTicketPdf(Long ticketId) throws IOException {
-
+        Optional<Ticket> optionalTicket = this.ticketRepository.findById(ticketId);
+        if (optionalTicket.isEmpty()) {
+            throw new NotFoundException(
+                String.format("Could not find ticket with id %s", ticketId));
+        }
+        Ticket ticket = optionalTicket.get();
+        ApplicationUser user = this.userRepository.findUserByEmail(
+            authenticationFacade.getAuthentication().getPrincipal().toString());
+        if (user.getUserId() != ticket.getPurchasedBy().getUserId()) {
+            //TODO: ADD CORRECT error
+            throw new NotFoundException(
+                String.format("Could not find ticket with id %s", ticketId));
+        }
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        PDDocument pdDocument = buildInvoicePdf();
+        PDDocument pdDocument = buildInvoicePdf(ticket, user);
         pdDocument.save(os);
         pdDocument.close();
         return new ByteArrayResource(os.toByteArray());
     }
 
-    public PDDocument buildInvoicePdf() throws IOException {
+    public PDDocument buildInvoicePdf(Ticket ticket, ApplicationUser user) throws IOException {
 
         float marginBody = 30;
 
@@ -111,10 +133,85 @@ public class TicketPrintServiceImpl implements TicketPrintService {
         cs.endText();
 
         yoffset -= 80;
+        cs.moveTo(xoffset, yoffset);
+        cs.lineTo(xend, yoffset);
+        cs.setStrokingColor(Color.BLACK);
+        cs.stroke();
+
+        yoffset -= 30;
+        cs.beginText();
+        cs.setLeading(20f);
+        cs.newLineAtOffset(xoffset, yoffset);
+        cs.setFont(plain, 14);
+        String seatingPlanName = ticket.getSeat().getSector().getSeatingPlan().getName();
+        Location location = ticket.getSeat().getSector().getSeatingPlan().getLocation();
+        cs.showText(location.getName() + " - " + seatingPlanName);
+        cs.newLine();
+        cs.setFont(plain, 20);
+        Event event = ticket.getShow().getEvent();
+        cs.showText(event.getName() + " (" + event.getCategory() + ")");
+        cs.newLine();
+        cs.setFont(plain, 12);
+        cs.showText("| ");
+        Set<Artist> artists = ticket.getShow().getArtists();
+        for (Artist artist : artists) {
+            if (artist.getFirstName() != null) {
+                cs.showText(artist.getFirstName() + " ");
+            }
+            if (artist.getLastName() != null) {
+                cs.showText(artist.getLastName() + " ");
+            }
+            if (artist.getBandName() != null) {
+                cs.showText(artist.getBandName() + " ");
+            }
+            cs.showText(" | ");
+        }
+        cs.endText();
+
+        cs.beginText();
+        cs.newLineAtOffset(xcompanydata, yoffset);
+        cs.setFont(plain, 10);
+        Address address = location.getAddress();
+        cs.showText(address.getStreet() + " " + address.getHouseNumber());
+        cs.newLine();
+        cs.showText(address.getZipCode() + " " + address.getCity() + ", " + address.getCountry());
+        cs.newLine();
+
+        cs.endText();
+
+        yoffset -= 60;
+        cs.moveTo(xoffset, yoffset);
+        cs.lineTo(xend, yoffset);
+        cs.setStrokingColor(Color.BLACK);
+        cs.stroke();
+
+        yoffset -= 30;
+        cs.beginText();
+        cs.setFont(bold, 14);
+        cs.setLeading(20f);
+        cs.newLineAtOffset(xoffset, yoffset);
+        OffsetDateTime showDate = ticket.getShow().getDate();
+        cs.showText(showDate.getDayOfWeek().toString() + ", " + showDate.getDayOfMonth() + ". "
+            + showDate.getMonth().toString() + " " + showDate.getYear() + " " + String.format(
+            "%02d", showDate.getHour()) + ":" + String.format("%02d", showDate.getMinute()));
+        cs.setFont(plain, 14);
+        Seat seat = ticket.getSeat();
+        if (seat.getRowNumber() != null || seat.getSeatNumber() != null) {
+            cs.newLine();
+        }
+        if (seat.getRowNumber() != null) {
+            cs.showText("Row " + seat.getRowNumber() + " ");
+        }
+        if (seat.getSeatNumber() != null) {
+            cs.showText("Seat " + seat.getSeatNumber());
+        }
+        cs.endText();
+
+        yoffset -= 30;
 
         cs.moveTo(xoffset, yoffset);
         cs.lineTo(xend, yoffset);
-        cs.setStrokingColor(Color.lightGray);
+        cs.setStrokingColor(Color.BLACK);
         cs.stroke();
         yoffset -= 30;
 
@@ -122,12 +219,39 @@ public class TicketPrintServiceImpl implements TicketPrintService {
         cs.setFont(bold, 14);
         cs.setLeading(20f);
         cs.newLineAtOffset(xoffset, yoffset);
-        cs.showText("Customer: ");
+        cs.showText("This ticket was bought by: ");
         cs.newLine();
         cs.setFont(plain, 14);
+        cs.showText(user.getLastName() + " " + user.getFirstName());
         cs.newLine();
-        //hier Kundendaten einfügen!
+        cs.showText(user.getAddress().getStreet() + " " + user.getAddress().getHouseNumber());
+        cs.newLine();
+        cs.showText(user.getAddress().getZipCode() + " " + user.getAddress().getCity() + ", "
+            + user.getAddress().getCountry());
+        cs.newLine();
+        cs.showText("and is only valid with a photographic identification.");
+        cs.newLine();
+        cs.newLine();
+        cs.setFont(bold, 14);
+        cs.showText("DISCLAIMER:");
+        cs.newLine();
+        cs.setFont(plain, 14);
+        cs.showText("This ticket is not allowed to be copied! Do not give away a copy/reprint!");
+        cs.newLine();
+        cs.showText(
+            "If the event is cancelled, the costs of the ticket is refunded automatically.");
+        cs.newLine();
+        cs.showText(
+            "Please keep the secret in good condition, it has to be readable at the entrance.");
+        cs.newLine();
+        cs.showText(
+            "Print this ticket on a A4 page (do not cut or manipulate it in any way).");
+        cs.newLine();
+        cs.showText(
+            "Only in this form the ticket is valid.");
         cs.endText();
+
+
         yoffset -= 20;
 
         cs.close();
