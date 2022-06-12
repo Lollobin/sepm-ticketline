e8b60b8f7b7eb0f400506f5b6643ed2b23c836ef
11909390 <benno.kossatz@tuwien.ac.at>
2022-06-12 19:16:49 +0200

    added duration formatting

--- frontend/src/app/components/top-events/top-events.component.ts
+++ frontend/src/app/components/top-events/top-events.component.ts
@@ -49,6 +49,17 @@ export class TopEventsComponent implements OnInit {
     });
   }
 
+  secondsToHms(d): string {
+    d = Number(d * 60);
+    const h = Math.floor(d / 3600);
+    const m = Math.floor(d % 3600 / 60);
+    const s = Math.floor(d % 3600 % 60);
+    const hDisplay = h > 0 ? h + (h === 1 ? " hour" : " hours") + (m > 0 || s > 0 ? ", " : "") : "";
+    const mDisplay = m > 0 ? m + (m === 1 ? " minute" : " minutes") + (s > 0 ? ", " : "") : "";
+    const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay;
+  }
+
   setError(error: any) {
     this.error = error;
   }
