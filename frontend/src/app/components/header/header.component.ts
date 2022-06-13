bf85a94c64b395db8879866bdfd79a9c747d858f
Christoph Baumann <e11907101@student.tuwien.ac.at>
2022-06-13 13:26:49 +0200

    Fixed: Frontend issues

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CustomAuthService} from '../../services/custom-auth.service';
 import {faEye, faUser} from "@fortawesome/free-solid-svg-icons";
-import { TicketsService } from 'src/app/generated-sources/openapi';
 
 @Component({
   selector: 'app-header',
