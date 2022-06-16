ed9d9afd11f1c680d52c3f47e32dbeb378a9bebb
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-16 16:33:24 +0200

    Added docs to interface

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/LockedService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/LockedService.java
@@ -2,5 +2,11 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 
 public interface LockedService {
 
+    /**
+     * Locks or unlocks a user based on the value of the body param.
+     *
+     * @param id   user with corresponding ID gets locked/unlocked
+     * @param body if true user gets locked, if false user gets unlocked
+     */
     void manageLockedStatus(Long id, Boolean body);
 }
