eeb327b775595cf208a9b0ebdfe66e2b8d93e199
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-16 14:28:46 +0200

    Added ability to lock user as administrator and cleaned up previous implementation

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/LockedService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/LockedService.java
@@ -2,11 +2,5 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 
 public interface LockedService {
 
-    /**
-     * Set lockedStatus of a user to false.
-     *
-     * @param id     id of the user whose status is being changed
-     * @param unlock set to false to unlock
-     */
-    void unlockApplicationUser(Long id, boolean unlock);
+    void manageLockedStatus(Long id, Boolean body);
 }
