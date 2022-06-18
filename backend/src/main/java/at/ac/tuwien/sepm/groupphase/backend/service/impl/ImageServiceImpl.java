bcaca23a62f5515b6030e2f09a3464cd4a62ecf5
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-19 00:13:53 +0200

    Added article generator with images

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ImageServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ImageServiceImpl.java
@@ -44,7 +44,6 @@ public class ImageServiceImpl implements ImageService {
         try {
 
             imageValidator.checkIfImageMimeTypeIsCorrect(body);
-
             toSave.setFilePath(fileSystemRepository.save(body.getBytes(),
                 body.getOriginalFilename()));
             toSave = imageRepository.save(toSave);
