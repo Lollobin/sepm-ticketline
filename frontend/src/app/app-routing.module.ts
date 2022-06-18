6b05b0391e895089c2dbc57294acb027d4f8d39b
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-06-18 11:44:05 +0200

    fixed error in app-routing.module.ts after merging

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {MessageComponent} from "./components/message/message.component";
