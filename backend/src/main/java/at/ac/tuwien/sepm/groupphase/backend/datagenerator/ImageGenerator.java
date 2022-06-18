4fa14e68593469d112e66ff69088d09df3e8b669
Anton Martinovic <e52004305@student.tuwien.ac.at>
2022-06-19 00:43:37 +0200

    Removed debug log

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/ImageGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/ImageGenerator.java
@@ -4,14 +4,11 @@ package at.ac.tuwien.sepm.groupphase.backend.datagenerator;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Image;
 import at.ac.tuwien.sepm.groupphase.backend.repository.FileSystemRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ImageRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -21,9 +18,6 @@ import java.util.Random;
 @Profile("generateData")
 @Component
 public class ImageGenerator {
-
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private static final File[] files = new File("src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/images").listFiles();
     private final ImageRepository imageRepository;
     private final FileSystemRepository fileSystemRepository;
@@ -46,7 +40,6 @@ public class ImageGenerator {
             String currentImageName = files[fileNum].getName();
             if (imageNames.contains(currentImageName)) {
                 i--;
-                LOGGER.debug("Same image would have been saved");
             } else {
 
                 imageNames.add(files[fileNum].getName());
