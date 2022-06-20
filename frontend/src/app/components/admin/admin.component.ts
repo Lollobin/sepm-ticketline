092c760c1e2374e48cad154a7672949328ea2ef8
11909390 <benno.kossatz@tuwien.ac.at>
2022-06-20 20:01:00 +0200

    reworked admin dashboard

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {CustomAuthService} from "../../services/custom-auth.service";
+import {
+  faAngleRight,
+  faCompactDisc,
+  faFileLines,
+  faMapLocation,
+  faUsers
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  arrow = faAngleRight;
+  event = faCompactDisc;
+  article = faFileLines;
+  users = faUsers;
+  location = faMapLocation;
 
-  constructor(public authService: CustomAuthService) { }
-
-  ngOnInit(): void {
+  constructor(public authService: CustomAuthService) {
   }
-
 }
