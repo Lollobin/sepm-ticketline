7b90a267d013d7c23e8175ac56fcfbf24df0d150
Filip Markovic <e12024750@student.tuwien.ac.at>
2022-06-25 19:28:43 +0200

    added seatingPlan preview on manageLocations add seatingPlans screen

--- frontend/src/app/components/location-seating-plans/location-seating-plans.component.ts
+++ frontend/src/app/components/location-seating-plans/location-seating-plans.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
-import { Location, LocationsService, SeatingPlan } from "src/app/generated-sources/openapi";
+import { Application } from "pixi.js";
+import { Location, LocationsService, SeatingPlan, SeatingPlanLayout, SeatingPlansService } from "src/app/generated-sources/openapi";
+import { drawSeatingPlanPreview } from "src/app/shared_modules/seatingPlanGraphics";
 
 @Component({
   selector: "app-location-seating-plans",
   templateUrl: "./location-seating-plans.component.html",
   styleUrls: ["./location-seating-plans.component.scss"],
 })
-export class LocationSeatingPlansComponent implements OnInit {
+export class LocationSeatingPlansComponent implements OnInit, AfterViewInit {
+  @ViewChild("pixiContainer") pixiContainer: ElementRef<HTMLDivElement>;
+  @ViewChild("infoOverlay") infoOverlay: ElementRef<HTMLDivElement>;
+
+  pixiApplication: Application;
+  
   locationId = 1;
   location: Location;
   seatingPlans: SeatingPlan[];
-  constructor(private route: ActivatedRoute, private locationsService: LocationsService, private toastr: ToastrService) {}
+  seatingPlanLayout: SeatingPlanLayout;
+  selectedSeatingPlan: SeatingPlan;
+  constructor(private route: ActivatedRoute, private locationsService: LocationsService, private toastr: ToastrService,
+    private seatingPlansService: SeatingPlansService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -47,5 +57,45 @@ export class LocationSeatingPlansComponent implements OnInit {
         }
       });
     });
+  }
+
+  ngAfterViewInit() {
+    this.pixiApplication = new Application({
+      antialias: true,
+      backgroundAlpha: 0,
+    });
+  }
+
+  initializeSeatingPlan() {
+    this.pixiApplication.stage.removeChildren();
+    this.pixiApplication.view.width = this.seatingPlanLayout.general.width;
+    this.pixiApplication.view.height = this.seatingPlanLayout.general.height;
+    document.addEventListener("mousemove", (event) => event);
+    this.pixiContainer.nativeElement.appendChild(this.pixiApplication.view);
+    drawSeatingPlanPreview(this.pixiApplication.stage, this.seatingPlanLayout);
+  }
+
+  numberToCssColorString(color: number) {
+    return `#${color.toString(16).padStart(6, "0")}`;
+  }
+
+  getSeatingPlanLayout(seatingPlan: SeatingPlan) {
+    this.selectedSeatingPlan = seatingPlan;
+    this.seatingPlansService
+      .seatingPlanLayoutsIdGet(seatingPlan.seatingPlanId)
+      .subscribe({
+        next: async (seatingPlanLayout) => {
+          this.seatingPlanLayout = seatingPlanLayout;
+          this.initializeSeatingPlan();
+        },
+        error: error => {
+          console.log("Error getting sectors of seatingPlanLayout with id", seatingPlan.seatingPlanId);
+          if (error.status === 0 || error.status === 500) {
+            this.toastr.error(error.message);
+          } else {
+            this.toastr.warning(error.error);
+          }
+        },
+      });
   }
 }
